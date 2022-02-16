e867ab4bd4f0833961313547ab3d35d01395b671
H_A _Opu <wwwhasib4@gmail.com>
2022-02-17 00:35:52 +0600

    getting all input using function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('heeeellloooo')
+document.getElementById('calculate-btn').addEventListener('click',function(){
+     const incomeInput = getInput('income-input');
+     console.log(incomeInput);
+     const foodInput = getInput('food-input');
+     console.log(foodInput);
+     const rantInput = getInput('rant-input');
+     console.log(rantInput);
+     const clothesInput = getInput('clothes-input');
+     console.log(clothesInput);
+     
+})
+
+function getInput(inputField) {
+    const input =  document.getElementById(inputField)
+    const totalIncomeText = parseInt(input.value)
+     input.value = '';
+    return totalIncomeText;
+}
