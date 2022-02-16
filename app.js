7ab28e90cf460e920c923d507dc29118c3d41c94
H_A _Opu <wwwhasib4@gmail.com>
2022-02-17 02:58:17 +0600

    getting saving and remaining amount

--- app.js
+++ app.js
@@ -10,25 +10,36 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
      const totalExpenses = foodInput + rantInput + clothesInput;
 
      const expensesTotal = document.getElementById('expenses-total');
-     const previousExpenses = parseInt(expensesTotal.innerText);
      expensesTotal.innerText = totalExpenses;
 
      const balanceTotal = incomeInput - expensesTotal.innerText;
      
      const totalBalance = document.getElementById('balance-total');
-     const previousBalance = parseInt(totalBalance.innerText);
-     totalBalance.innerText = previousBalance + balanceTotal;
+    //  const previousBalance = parseInt(totalBalance.innerText);
+     totalBalance.innerText =  balanceTotal;
      
 })
 
+document.getElementById('save-btn').addEventListener('click', function(){
+    const saving = document.getElementById('saving-input');
+     const totalBalance = document.getElementById('balance-total');
+    const savingAmount = document.getElementById('saving-amount');
+    const remainingBalance = document.getElementById('remaining-balance')
+    const totalSaving = (totalBalance.innerText*saving.value)/100;
+    savingAmount.innerText = totalSaving;
+    const remainingAmount = totalBalance.innerText - savingAmount.innerText;
+    remainingBalance.innerText = remainingAmount;
+})
+
 function getInput(inputField) {
-    const input =  document.getElementById(inputField)
-    input.value = '';
-    if(isNaN(input) || 0> input ){
+    let input =  document.getElementById(inputField)
+    let totalInputValue = parseInt(input.value);
+    
+    if(isNaN(totalInputValue) || totalInputValue <0){
         console.log('number deya!!');
-        return alert('plzz..give a number')
+        input.value = '';
+        return alert('plzz..')
     }
-    const totalIncomeText = parseInt(input.value)
-   
-    return totalIncomeText;
+    input.value = '';
+    return totalInputValue;
 }
